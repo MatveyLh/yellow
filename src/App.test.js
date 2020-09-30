dbda7d598c127598de7c3a52358ff8e642dcc869
MatveyLh <liakh.matvei@mail.ru>
2020-09-30 10:16:58 +0300

    add simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import Jogs from "./components/Jogs/Jogs";
+import Header from "./components/Header/Header";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button let me in', () => {
+ render(<App />);
+ expect(screen.getByRole('button')).toBeInTheDocument()
 });
+test('check empty list', () => {
+  render(<Jogs/>);
+  expect(screen.getByText(/nothing is there/i)).toBeInTheDocument()
+})
